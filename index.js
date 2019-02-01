2c6a84e0612697c9da45bf0a4a8929a37b6434c2
V. Venkata Narayanan <venkatanarayanan.v@mookambikainfo.com>
2019-02-01 13:57:03 +0530

    Fine Bote

---
+++ index.js
@@ -0,0 +1,13 @@
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://root:admin123@ds117545.mlab.com:17545/hexa';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
