b976919d2f2a4ff13b2a3f6b65d434f859789eb5
V. Venkata Narayanan <venkatanarayanan.v@mookambikainfo.com>
2019-02-01 15:36:55 +0530

    DB Add'

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ mongoose.connect(mongoDB, { useNewUrlParser: true });
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
-var db = mongoose.connection;
+var con = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+con.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var userData = mongoose.Schema({
+    name : { type: String, required: true, trim: true },
+    amount : { type: String, trim: true }
+});
+
+var user = mongoose.model('user', userData, 'EnglishFine');
+con.once('open', function () {
+
+	/*var user1 = new user({
+		name: "Surya",
+		amount: "100"
+	});*/
+	/*user1.save(function (err, user) {
+      if (err) return console.error(err);
+      console.log(user.amount + 'rs is Added to ' + user.name + ' :)');
+
+		con.db.collection("EnglishFine", function(err, collection){
+			collection.find({name: 'surya'}).toArray(function(err, data){
+			    console.log(data); 
+			});
+		});
+
+    });*/
+
+    con.db.collection("EnglishFine", function(err, collection){
+			collection.find({name:"Surya"}).toArray(function(err, data){
+			    console.log(data); 
+			});
+		});
+
+});
